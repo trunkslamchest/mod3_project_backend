416c78002a439dfa15cf554b0c96e97e9fc5dda8
trunkslamchest <trunk9slamchest@gmail.com>
2020-03-23 12:31:10 -0400

    inital push to new branch

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
 User.destroy_all
 
-numbers = (1..1000).to_a
-months = [
-	"Jaunary",
-	"Februrary",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-]
+# numbers = (1..1000).to_a
+# months = [
+# 	"Jaunary",
+# 	"Februrary",
+# 	"March",
+# 	"April",
+# 	"May",
+# 	"June",
+# 	"July",
+# 	"August",
+# 	"September",
+# 	"October",
+# 	"November",
+# 	"December"
+# ]
 
 User.create(
 	user_name: "admin1",
@@ -34,29 +34,29 @@ User.create(
 	zip_code: 69420
 )
 
-10.times {
-	User.create(
-		user_name: "user#{numbers.sample}",
-		password: "pw",
-		email: Faker::Internet.free_email,
-		first_name: Faker::Name.first_name,
-		last_name: Faker::Name.last_name,
-		gender: Faker::Gender.binary_type,
-		birth_day: rand(1..28),
-		birth_month: "#{months.sample}",
-		birth_year: rand(1950..2005),
-		house_number: rand(1..999),
-		street_name: "#{Faker::Address.street_name} #{Faker::Address.street_suffix}",
-		city_town: Faker::Address.community,
-		state: Faker::Address.state,
-		zip_code: rand(10000..99999)
-	)
-}
+# 10.times {
+# 	User.create(
+# 		user_name: "user#{numbers.sample}",
+# 		password: "pw",
+# 		email: Faker::Internet.free_email,
+# 		first_name: Faker::Name.first_name,
+# 		last_name: Faker::Name.last_name,
+# 		gender: Faker::Gender.binary_type,
+# 		birth_day: rand(1..28),
+# 		birth_month: "#{months.sample}",
+# 		birth_year: rand(1950..2005),
+# 		house_number: rand(1..999),
+# 		street_name: "#{Faker::Address.street_name} #{Faker::Address.street_suffix}",
+# 		city_town: Faker::Address.community,
+# 		state: Faker::Address.state,
+# 		zip_code: rand(10000..99999)
+# 	)
+# }
 
 Player.destroy_all
 Scoreboard.destroy_all
 
-25.times{Player.create(name: Faker::Internet.username)}
+# 25.times{Player.create(name: Faker::Internet.username)}
 
-20.times{Scoreboard.create(player_id: Player.ids.sample, score: rand(1..200), power_level: rand(0.00001..2.50001).floor(5))}
+# 20.times{Scoreboard.create(player_id: Player.ids.sample, score: rand(1..2), power_level: rand(0.00001..2.50001).floor(5))}
 
